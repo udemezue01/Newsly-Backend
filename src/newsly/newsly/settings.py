be137b321d1b233e6ab3d5f00de0974a0e90b2db
Udemezue John <johnzeus14@gmail.com>
2024-08-19 13:30:10 +0100

    added post and profile apps

--- src/newsly/newsly/settings.py
+++ src/newsly/newsly/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'post',
+    'userprofile',
+
+    #Third-Party
+    'graphene_django',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +127,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#GraphQL Schema
+
+GRAPHENE = {
+    "SCHEMA": "newsly.schema.schema"
+}
+
+
