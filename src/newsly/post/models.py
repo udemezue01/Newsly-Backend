9f784e82180c22355976b6a5149587201bae2d2e
Udemezue John <johnzeus14@gmail.com>
2024-08-21 08:22:50 +0100

    Added Post and Tag Models

--- src/newsly/post/models.py
+++ src/newsly/post/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Tag(models.Model):
+
+    title        = models.CharField(max_length=3000)
+
+    slug         = models.SlugField(auto_created=True)
+
+
+class Post(models.Model):
+
+    user                  = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
+
+    title                 = models.CharField(max_length=3000, blank=True)
+    content               = models.TextField(blank=True)
+    tags                  = models.ManyToManyField()
+
+    featured_Image        = models.FileField(blank=True)
+
+    created_at            = models.DateTimeField(auto_now=True, auto_now_add=True)
+
+    def __str__(self):
+        return self.title
